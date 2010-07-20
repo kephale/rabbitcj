4114e78ec3ab1ef54c503d94bd380802e19f251a
Bradford Cross <bradford@bradford-laptop.(none)>
2010-07-20 16:53:19 -0700

    more client examples.

--- test/rabbitcj/client_test.clj
+++ test/rabbitcj/client_test.clj
@@ -23,3 +23,91 @@
 ;;         (let [rabbit (r/consume chan consumer 1000)]
 ;;           (println (inc i) rabbit))))
 ;;     (println "rabbits left:" (r/message-count chan "rabbits"))))
+
+;; (defn random-id []
+;;   (str (java.util.UUID/randomUUID)))
+
+;; (defn bootstrap-consumer
+;;   "Declares an exchange and a queue, then binds them together."
+;;   [channel exch exch-type queue id]
+;;   (let [consumer (QueueingConsumer. channel)]
+;;     (doto channel
+;;       (.exchangeDeclare exch exch-type)
+;;       (.queueDeclare queue)
+;;       (.basicConsume queue consumer)
+;;       (.queueBind queue exch id)) consumer))
+
+;; (defn fan-out-using-keys []
+;;   (with-open [connection (connect host)
+;;               channel (.createChannel connection)]
+;;     (let [queue-name (random-id)
+;;           queue-name2 (random-id)
+;;           id (random-id)
+;;           exchange-name (random-id)
+;;           consumer  (bootstrap-consumer channel exchange-name direct-exchange queue-name id)
+;;           consumer2 (bootstrap-consumer channel exchange-name direct-exchange queue-name2 id)]
+;;       (publish channel exchange-name id "hello")
+;;       (str (consume consumer channel) "  " (consume consumer2 channel)))))
+
+;; (defn fan-out-using-fanout-exchange []
+;;   (with-open [connection (connect host)
+;;               channel (.createChannel connection)]
+;;     (let [queue-name (random-id)
+;;           queue-name2 (random-id)
+;;           id (random-id)
+;;           exchange-name (random-id)
+;;           consumer  (bootstrap-consumer channel exchange-name fanout-exchange queue-name "boz")
+;;           consumer2 (bootstrap-consumer channel exchange-name fanout-exchange queue-name2 "biz")]
+;;       (publish channel exchange-name "baz" "hello")
+;;       (str (consume consumer channel) "  " (consume consumer2 channel)))))
+
+;; (defn two-independent-queues []
+;;   (with-open [connection (connect host)
+;;               channel (.createChannel connection)]
+;;     (let [queue-name (random-id)
+;;           queue-name2 (random-id)
+;;           id (random-id)
+;;           exchange-name (random-id)
+;;           exchange-name2 (random-id)
+;;           consumer  (bootstrap-consumer channel exchange-name direct-exchange queue-name id)
+;;           consumer2 (bootstrap-consumer channel exchange-name2 direct-exchange queue-name2 id)]
+;;       (publish channel exchange-name id "hello")
+;;       (publish channel exchange-name2 id "goodbye")
+;;       (str (consume consumer channel) "  " (consume consumer2 channel)))))
+
+
+;; (defn service-master []
+;;   (react-to-commands (new-commands (fetch-commands master-username))))
+
+;; (defn queues-view []
+;;   (html [:p "There are " (count-feeds) " urls on the queue."]))
+
+;;  [:get "/queues"]
+;;       (response/response (queues-view))
+
+;; (defvar bot-exchange
+;;   "bot")
+
+;; (defvar fetch-and-tweet-queue
+;;   "fetch-and-tweet")
+
+;; (defn declare-queues [clear]
+;;   (with-open [conn (rmq/connect rabbitmq-params)]
+;;     (with-open [chan (rmq/create-channel conn)]
+;;       (rmq/declare-exchange chan bot-exchange rmq/direct-exchange true)
+;;       (when clear
+;;         (rmq/delete-queue chan fetch-and-tweet-queue))
+;;       (rmq/declare-queue chan fetch-and-tweet-queue true)
+;;       (rmq/bind-queue chan fetch-and-tweet-queue bot-exchange fetch-and-tweet-queue))))
+
+;; (defvar rabbitmq-params
+;;   {:username     "guest"
+;;    :password     "guest"
+;;    :host         "127.0.0.1"
+;;    :port         5672
+;;    :virtual-host "/"})
+
+;; (deftest test-enqueue-feeds
+;;   (bot/declare-queues true)
+;;   (bot/enqueue-feeds)
+;;   (is (= 105 (bot/count-feeds))))
